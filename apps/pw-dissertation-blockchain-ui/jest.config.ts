8f1bbd5a1f25a1c6896f448eebaf2d46e9d6744d
illiamishkin <iluhamishkin@gmail.com>
2024-05-24 18:00:24 +0200

    Initial commit

---
+++ apps/pw-dissertation-blockchain-ui/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'pw-dissertation-blockchain-ui',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/pw-dissertation-blockchain-ui',
+};
